ea8d4b0feabcd0f76aa6b6047341fae00aec7300
aweasdwesad <87714275+aweasdwesad@users.noreply.github.com>
2022-05-02 02:02:36 +0100

    Update fu.lua

--- fu.lua
+++ fu.lua
@@ -1,1 +1,23 @@
-ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘k 
+Pron.MouseButton1Down:Connect(function()
+	byy("pr1on")
+end)
+
+Fuck.MouseButton1Down:Connect(function()
+	byy(fu)
+end)
+
+FuckYou.MouseButton1Down:Connect(function()
+	byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘k  Yo  ðŒ‘ Íœ Í£ u ")
+end)
+
+FuckingRetard.MouseButton1Down:Connect(function()
+	byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘kðŒ‘ðŒ‘ðŒ‘iðŒ‘ðŒ‘ðŒ‘nðŒ‘ðŒ‘ðŒ‘g  Re ðŒ‘ Íœ Í£ ta ðŒ‘ Íœ Í£ rd ")
+end)
+
+
+
+
+Bitch.MouseButton1Down:Connect(function()
+	byy(" Bi ðŒ‘ Íœ Í£ tc  ðŒ‘ Íœ Í£ h ")
+end)
+
