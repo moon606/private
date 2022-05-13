2a1b3ca70f018850949103b9b5a5ccf4e74ffde7
aweasdwesad <87714275+aweasdwesad@users.noreply.github.com>
2022-05-13 16:24:44 +0100

    Update fu.lua

--- fu.lua
+++ fu.lua
@@ -1,23 +1,30 @@
-Pron.MouseButton1Down:Connect(function()
-	byy("pr1on")
+
+
+local gui = game:GetService("CoreGui"):FindFirstChild("bloxton"):FindFirstChild("Frame"):FindFirstChild("chatbypassf")
+
+function byy(whatthefuck)
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(whatthefuck,"All")
+end
+
+
+
+
+
+gui.Fuck.Button.MouseButton1Down:Connect(function()
+	byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘k ")
 end)
 
-Fuck.MouseButton1Down:Connect(function()
-	byy(fu)
-end)
-
-FuckYou.MouseButton1Down:Connect(function()
+gui.Fuckyou.Button.MouseButton1Down:Connect(function()
 	byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘k  Yo  ðŒ‘ Íœ Í£ u ")
 end)
 
-FuckingRetard.MouseButton1Down:Connect(function()
+gui.FuckingRetard.Button.MouseButton1Down:Connect(function()
 	byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘kðŒ‘ðŒ‘ðŒ‘iðŒ‘ðŒ‘ðŒ‘nðŒ‘ðŒ‘ðŒ‘g  Re ðŒ‘ Íœ Í£ ta ðŒ‘ Íœ Í£ rd ")
 end)
 
 
 
 
-Bitch.MouseButton1Down:Connect(function()
+gui.Bitch.Button.MouseButton1Down:Connect(function()
 	byy(" Bi ðŒ‘ Íœ Í£ tc  ðŒ‘ Íœ Í£ h ")
 end)
-
