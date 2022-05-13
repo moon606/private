f6bb501cb0fc4693e221d19b4965e2ba452340cd
aweasdwesad <87714275+aweasdwesad@users.noreply.github.com>
2022-05-13 16:40:39 +0100

    Update fu.lua

--- fu.lua
+++ fu.lua
@@ -1,6 +1,5 @@
 
 
-local gui = game:GetService("CoreGui"):WaitForChild("bloxton"):FindFirstChild("Frame"):FindFirstChild("chatbypassf")
 
 function byy(whatthefuck)
 	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(whatthefuck,"All")
@@ -10,21 +9,23 @@ end
 
 
 
-gui.Fuck.Button.MouseButton1Down:Connect(function()
-	byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘k ")
-end)
+for i,v in pairs(game:GetService("CoreGui"):WaitForChild("bloxton"):GetDescendants()) do
 
-gui.Fuckyou.Button.MouseButton1Down:Connect(function()
-	byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘k  Yo  ðŒ‘ Íœ Í£ u ")
-end)
-
-gui.FuckingRetard.Button.MouseButton1Down:Connect(function()
-	byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘kðŒ‘ðŒ‘ðŒ‘iðŒ‘ðŒ‘ðŒ‘nðŒ‘ðŒ‘ðŒ‘g  Re ðŒ‘ Íœ Í£ ta ðŒ‘ Íœ Í£ rd ")
-end)
-
-
-
-
-gui.Bitch.Button.MouseButton1Down:Connect(function()
-	byy(" Bi ðŒ‘ Íœ Í£ tc  ðŒ‘ Íœ Í£ h ")
-end)
+	if v.Name == "Fuck"  and v:IsA("ImageLabel") and v:FindFirstChild("Button") then
+		v.Button.MouseButton1Down:Connect(function()
+			byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘k ")
+		end)
+	elseif v.Name == "Fuckyou"  and v:IsA("ImageLabel") and v:FindFirstChild("Button") then
+		v.Button.MouseButton1Down:Connect(function()
+byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘k  Yo  ðŒ‘ Íœ Í£ u ")
+		end)
+	elseif v.Name == "FuckingRetard"  and v:IsA("ImageLabel") and v:FindFirstChild("Button") then
+		v.Button.MouseButton1Down:Connect(function()
+byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘kðŒ‘ðŒ‘ðŒ‘iðŒ‘ðŒ‘ðŒ‘nðŒ‘ðŒ‘ðŒ‘g  Re ðŒ‘ Íœ Í£ ta ðŒ‘ Íœ Í£ rd ")
+		end)
+	elseif v.Name == "Bitch"  and v:IsA("ImageLabel") and v:FindFirstChild("Button") then
+		v.Button.MouseButton1Down:Connect(function()
+byy(" Bi ðŒ‘ Íœ Í£ tc  ðŒ‘ Íœ Í£ h ")
+		end)
+	end
+end
