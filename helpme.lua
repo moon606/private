beb8436a8924c3447737a522dc232534fa754a9b
aweasdwesad <87714275+aweasdwesad@users.noreply.github.com>
2022-05-13 16:46:15 +0100

    Create helpme.lua

---
+++ helpme.lua
@@ -0,0 +1,31 @@
+
+
+
+function byy(whatthefuck)
+	game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest:FireServer(whatthefuck,"All")
+end
+
+
+
+
+
+for i,v in pairs(game.Players.LocalPlayer:WaitForChild("PlayerGui"):WaitForChild("bloxton"):GetDescendants()) do
+
+	if v.Name == "Fuck"  and v:IsA("ImageLabel") and v:FindFirstChild("Button") then
+		v.Button.MouseButton1Down:Connect(function()
+			byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘k ")
+		end)
+	elseif v.Name == "Fuckyou"  and v:IsA("ImageLabel") and v:FindFirstChild("Button") then
+		v.Button.MouseButton1Down:Connect(function()
+byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘k  Yo  ðŒ‘ Íœ Í£ u ")
+		end)
+	elseif v.Name == "FuckingRetard"  and v:IsA("ImageLabel") and v:FindFirstChild("Button") then
+		v.Button.MouseButton1Down:Connect(function()
+byy("ffðŒ‘ðŒ‘ðŒ‘uðŒ‘ðŒ‘ðŒ‘cðŒ‘ðŒ‘ðŒ‘kðŒ‘ðŒ‘ðŒ‘iðŒ‘ðŒ‘ðŒ‘nðŒ‘ðŒ‘ðŒ‘g  Re ðŒ‘ Íœ Í£ ta ðŒ‘ Íœ Í£ rd ")
+		end)
+	elseif v.Name == "Bitch"  and v:IsA("ImageLabel") and v:FindFirstChild("Button") then
+		v.Button.MouseButton1Down:Connect(function()
+byy(" Bi ðŒ‘ Íœ Í£ tc  ðŒ‘ Íœ Í£ h ")
+		end)
+	end
+end
