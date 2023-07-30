6fc954a2668d31a4b190c228467274dd79802b78
old account <53540146+oldAccount894@users.noreply.github.com>
2023-07-30 02:44:56 +0100

    Update Standard.lua

--- Standard.lua
+++ Standard.lua
@@ -1,6 +1,5 @@
 local API = {}
 
-local dir = script.Parent
 
 API.basic = loadstring(game:HttpGet("https://raw.githubusercontent.com/oldAccount894/private/main/stdfunctions.lua"))() -- gets Standard functions (exists like this so .Math can require the stdfunctions module without recursion)
 API.math = loadstring(game:HttpGet("https://raw.githubusercontent.com/oldAccount894/private/main/Math.lua"))() -- Math Library
