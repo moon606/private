d76f81c9ce8dc3107e3cf796d6e3958c8416f5f3
old account <53540146+oldAccount894@users.noreply.github.com>
2023-08-13 03:19:17 +0100

    Update hi.lua

--- hi.lua
+++ hi.lua
@@ -26,6 +26,9 @@ end
 if not pcall(function() readfile(Name) end) then writefile("Returnerbot.hi", game:service'HttpService':JSONEncode(DefaultSettings)) end
 Settings = game:service'HttpService':JSONDecode(readfile("Returnerbot.hi"))
 if Settings.botgoingto  then
+	  if Settings.botgoingto == nil then
+        print("nil")
+        else
 	print("rejoined")
 	botgoing = Settings.botgoingto
 	Settings.rejoined = "f"
